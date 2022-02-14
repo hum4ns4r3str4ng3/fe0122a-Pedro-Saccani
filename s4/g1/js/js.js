d35715acbb58a3e7fe78481fe13727d965279b08
hum4ns4r3str4ng3 <psaccani96@gmail.com>
2022-02-14 12:04:56 +0000

    s4g1

---
+++ s4/g1/js/js.js
@@ -0,0 +1,24 @@
+
+console.log('Hello World');
+
+window.alert ('Hello World');
+
+document.write('<h1 id="color">Hello, World!</h1>'); 
+
+
+function changeColor() {
+    document.getElementById('colore').style.color = '#FF0000';
+    document.getElementById('color').style.color = '#FF0000';
+}
+
+
+function changeSize() {
+    document.getElementById('colore').style.fontSize = '30px';
+    document.getElementById('color').style.fontSize = '30px';
+}
+
+function changeText() {
+    document.getElementById('colore').document.write ('questo e un altro texto');
+    document.getElementById('color').style.fontSize = '30px';
+}
+
