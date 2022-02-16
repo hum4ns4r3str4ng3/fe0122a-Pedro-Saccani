0a87e76636e6afa49aba22fb0354908532a67086
hum4ns4r3str4ng3 <psaccani96@gmail.com>
2022-02-16 10:48:04 +0000

    s4g3

---
+++ s4/g3/javascript/script.js
@@ -0,0 +1,35 @@
+
+var eta = (anni>18) ? "proibito l ingresso" : "ingresso permesso"; 
+
+
+var anni = 24 ;
+
+document.write(eta);
+
+
+// CHIEDERE perche quando cambia variabile aggiorna valore di tutti due, e come fare a spostare su una nuova linea //
+
+vitalunga = () => "Buongiorno";
+alert(vitalunga()) ;
+
+// STEP 2 senza domanda //
+
+
+function operazione (){
+    var primonumero = 5 ;
+    var secondonumer = 8;
+    function multiplicazione ()  {
+        console.log(primonumero * secondonumer);
+    }
+    return multiplicazione();
+
+}
+
+operazione();
+
+// 1 definire funzione , 2 definire variabile, 3 definire altra funzione 4 return, 5 scrivere la prima funzione //
+
+
+
+alert(8!=10);
+alert(8==10); 
